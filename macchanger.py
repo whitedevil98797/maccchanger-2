1ae14cb2b5dc57f736dfd1e7a706bb4ddffdfc57
whitedevil98797 <154520882+whitedevil98797@users.noreply.github.com>
2023-12-21 17:46:17 +0530

    macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -9,7 +9,6 @@ interface = input("enter your interface name : ") #ex-wlan0 or eth0
 subprocess.run(["ifconfig", interface,"down"]) 
 subprocess.run(["ifconfig" , interface ,"hw", "ether", new_mac ])
 subprocess.run(["ifconfig" , interface , "up"])
-
 print("your mac is changed successfully") 
 
 
